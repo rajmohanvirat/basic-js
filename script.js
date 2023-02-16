fa091edcaaa8ccd28c1b5c1ebd65f3a18c36b7cb
rajmohanvirat <mohanvirat365@gmail.com>
2023-02-16 13:33:52 +0530

    basic  of  js

---
+++ script.js
@@ -0,0 +1,108 @@
+// let country="india";
+// let continent="Asia";
+// let population="200 million";
+// console.log( country)
+// console.log(continent)
+// console.log(population)
+
+// concatination sample
+
+let language="tamil";
+let country="india";
+let continent="Asia";
+let population="200 ";  
+const description1 = 
+country  + " is in " +
+continent + ", and its " + 
+population + " million people speak " +
+language;
+
+console.log(description1);
+// console.log( country)
+// console.log(continent)
+// console.log(population)
+
+
+
+// // const country="india";
+// // const continent="Asia";
+// // const population="200 million";
+// // console.log(country)
+// // console.log(continent)
+// // console.log(population)
+
+// // let language="tamil";
+// // console.log(language)
+
+// let island='false';
+// console.log(typeof island);
+
+// const language="tamil";
+// const nation=true;
+// const state="tamilnadu";
+// const count="200";
+
+// console.log(typeof language);
+// console.log(typeof nation)
+// console.log(typeof count)
+
+
+
+//BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+  //  and height in meter)
+    //test1
+
+    // const markMass =78;
+    // const markHeight =1.69;
+
+    // const johnMass =95;
+    // const johnHeight =1.88;
+
+    // const markBMI = markMass / (markHeight * markHeight);
+    // const johnBMI = johnMass / (johnHeight * johnHeight);
+
+    // console.log(markMass);
+    // console.log(markBMI);
+    // console.log(johnBMI);
+
+    //test case2
+
+    // const markWeight =92 ;
+    // const markHeight =1.95 ;
+
+    // const johnWeight=85;
+    // const johnHeight=7.6;
+
+    // const markBMI= markWeight / markHeight ** 2;
+    // const johnBMI= johnWeight / johnHeight ** 2;
+    // if(markBMI > johnBMI ) {
+    //     console.log("Mark's BMI is higher than John's!");
+
+    // }
+    // else{
+    //     console.log("John's BMI is higher than Mark's!");
+    // }
+    
+    
+    // console.log(markBMI);
+    // console.log(johnBMI);
+
+    
+    // const markWeight =92 ;
+    // const markHeight =1.95 ;
+
+    // const johnWeight=85;
+    // const johnHeight=7.6;
+
+    // const markBMI= markWeight / markHeight ** 2;
+    // const johnBMI= johnWeight / johnHeight ** 2;
+    // if(markBMI > johnBMI ) {
+    //     console.log(`Mark's BMI ${markBMI.toFixed(1)} is higher than John's ${johnBMI.toFixed(1)}!`);
+    // }
+    // else{
+    //     console.log("John's BMI is higher than Mark's!");
+    // }
+    
+    
+    // console.log(markBMI);
+    // console.log(johnBMI);
